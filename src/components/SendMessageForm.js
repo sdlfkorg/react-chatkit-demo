f692a0056dc93a230e8139de94a76906a454f275
sdlfkorg <sdlfkorg@gmail.com>
2019-07-25 12:32:24 +0800

    SendMessageForm: set basic structure of the component

--- src/components/SendMessageForm.js
+++ src/components/SendMessageForm.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react'
 
 export class SendMessageForm extends Component {
+  constructor() {
+    super()
+    this.state = {
+        message: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    
+  }
+
+  handleChange(e) {
+    this.setState({
+        message: e.target.value
+    })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    console.log(this.state.message)
+  }
+
+
   render() {
     return (
-      <div>
-        SendMessageForm works!
-      </div>
+      <form
+        onSubmit={this.handleSubmit}
+        className="send-message-form"
+      >
+        <input
+          onChange={this.handleChange}
+          value={this.state.message}
+          placeholder="Type your message and hit ENTER"
+          type="text" />
+      </form>
     )
   }
 }
